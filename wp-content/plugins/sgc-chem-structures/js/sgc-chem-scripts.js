fa9e2b52f488253938a797469b7958db008c660c
Leonidas Koukouflis <leonidas.koukouflis@sgc.ox.ac.uk>
2019-06-20 16:48:50 +0100

    Export 20190620.

--- wp-content/plugins/sgc-chem-structures/js/sgc-chem-scripts.js
+++ wp-content/plugins/sgc-chem-structures/js/sgc-chem-scripts.js
@@ -43,93 +43,97 @@ jQuery( document ).ready(function() {
 			molBlockReplaced = molBlockReplaced.replace(/ /g, "|7777|");
 			
 			var sim_threshold =  jQuery('#threshold').val();
-			console.log(sim_threshold);
 			
 			//Search request			
 			var url = 'https://thesgc.github.io/static-openlabnotebooks/wp-content/plugins/sgc-chem-structures/sgc-chem-search.php';
 			var params = 'molblock=' + molBlockReplaced + '&threshold=' + sim_threshold;
-			//console.log(params);
 			var http = new XMLHttpRequest();
 			http.open('POST', url, true);
 			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			var loaderIcon = document.getElementById("loading");
 			
 			http.onreadystatechange = function() {
 				if(this.readyState == 4 && this.status == 200) {
-			
-					//Generate results table
-					//console.log('Response: ' + http.response);
-					var searchResults = document.getElementById('search-results');
-
-				  var table = document.createElement('table');
-					
-					var thead = document.createElement('thead');
-					thead.innerHTML = '<tr><th>Structure</th><th>Title</th><th>Author</th><th>Catgeories</th></tr>';		
-					
-					var tbdy = document.createElement('tbody');
-					
+					loaderIcon.innerHTML = '';
 					var results_obj = JSON.parse(http.responseText);
-					for (var key in results_obj) {
-						var single_compound  = results_obj[key];
-						console.log(single_compound);
+					var searchResults = document.getElementById('search-results');
+					
+					if (results_obj.length > 0) { //Check if we get results
 						
-						if (single_compound.error == 'Internal error - please contact the system administrator') {
-							alert('Error');
-							return;
-						}
+						//Generate results table
+						var table = document.createElement('table');
 						
-						var image = single_compound.png_image;
-						var postID = single_compound.elnid;
-						var title = single_compound.title;		
+						var thead = document.createElement('thead');
+						thead.innerHTML = '<tr><th>Structure</th><th>Title</th><th>Author</th><th>Categories</th></tr>';		
+						
+						var tbdy = document.createElement('tbody');
+
+						for (var key in results_obj) {
+							var single_compound  = results_obj[key];
+							
+							if (single_compound.error == 'Internal error - please contact the system administrator') {
+								alert('Error');
+								return;
+							}
+							
+							var image = single_compound.png_image;
+							var postID = single_compound.elnid;
+							var title = single_compound.title;		
 							var tr = document.createElement('tr');
 					
 					
-								var td1 = document.createElement('td');
-								var img = document.createElement("img");
-								img.src = 'data\:image\/png;base64,' + image;
-								td1.appendChild(img);
-								
-								var td2 = document.createElement('td');
-								
-								var link = document.createElement('a');
-								link.href = window.location.host + '/index.php?page_id=' + postID;
-								td2.appendChild(link);
+							var td1 = document.createElement('td');
+							var img = document.createElement("img");
+							img.src = 'data\:image\/png;base64,' + image;
+							td1.appendChild(img);
 							
+							var td2 = document.createElement('td');
+							
+							var link = document.createElement('a');
+							link.href = window.location.host + '/index.php?page_id=' + postID;
+							td2.appendChild(link);
+						
+							td2.setAttribute('valign', 'middle');
+							td2.style.verticalAlign = 'middle';
+							link.appendChild(document.createTextNode(title));
+							
+							
+							tr.appendChild(td1);
+							tr.appendChild(td2);
 
-								td2.setAttribute('valign', 'middle');
-								td2.style.verticalAlign = 'middle';
-	
-								link.appendChild(document.createTextNode(title));
-								
-								
-								tr.appendChild(td1);
-								tr.appendChild(td2);
+							var authorTd = document.createElement('td');
+							authorTd.appendChild(document.createTextNode(single_compound.author));
+							tr.appendChild(authorTd);
+							authorTd.setAttribute('valign', 'middle');
+							authorTd.style.verticalAlign = 'middle';
 
-								var authorTd = document.createElement('td');
-								authorTd.appendChild(document.createTextNode(single_compound.author));
-								tr.appendChild(authorTd);
-								authorTd.setAttribute('valign', 'middle');
-								authorTd.style.verticalAlign = 'middle';
-
-								var catTd = document.createElement('td');
-								catTd.appendChild(document.createTextNode(single_compound.categories));
-								tr.appendChild(catTd);
-								catTd.setAttribute('valign', 'middle');
-								catTd.style.verticalAlign = 'middle';
-
+							var catTd = document.createElement('td');
+							catTd.appendChild(document.createTextNode(single_compound.categories));
+							tr.appendChild(catTd);
+							catTd.setAttribute('valign', 'middle');
+							catTd.style.verticalAlign = 'middle';
 							
 							tbdy.appendChild(tr);
+						}
+						
+						table.appendChild(thead);
+						table.appendChild(tbdy);
+						searchResults.innerHTML = '<span id="loading"></span>';
+						searchResults.appendChild(table);
 					}
 					
-					table.appendChild(thead);
-					table.appendChild(tbdy);
-					searchResults.innerHTML = '';
-					searchResults.appendChild(table);
-					
+					else {
+						searchResults.innerHTML = '<span id="loading"></span>';
+						noResults = document.createElement('h4');
+						noResults.textContent = 'No results were found.';
+						searchResults.appendChild(noResults);
+					}
 				}
 			}
+			
+			loaderIcon.innerHTML = '<img src="/wp-content/plugins/sgc-chem-structures/img/spinner.gif" />';
 			http.send(params);
-
-
+			
 		}
 	});
 
